4d5e155a390d7e594d8f8d16bb93ecb01d2a1333
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-03-11 23:40:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Simple Interest/simple-interest.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    double simpleInterest(int P, int R, int T) {
+        // code here
+        double intrest = (R*P/100.0)*T;
+        return intrest;
+        
+    }
+}
