a44718a9d2c62c2ef19c98f48f9f2bad6193578e
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-03-12 09:50:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print In Python/print-in-python.py
+++ Difficulty: Easy/Print In Python/print-in-python.py
@@ -1,19 +1,22 @@
 #User function Template for python3
 a = input()
 b = input()
-separator = input()
+c = input()
 
 ########### Write your code below ###############
 
+
+
+
+
 # Print with space
-
+print(a,b)
 # Print without newline at the end
-
+print(a,b+a+c+b)
+print(a+b)
 # Print with separator
 
 # Print without space
 
 ########### Write your code above ###############
-print(a , b)
-print(a,b+a+separator+b)
-print(a+b)
+
