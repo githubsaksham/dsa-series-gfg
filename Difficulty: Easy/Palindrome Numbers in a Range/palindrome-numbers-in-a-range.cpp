f311082fc462361c1f39e2f0733a3fc20338e64e
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-07-11 11:11:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Numbers in a Range/palindrome-numbers-in-a-range.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> printPalindromes(int m, int n) {
+        // code here
+        vector<int> result;
+        for(int i=m;i<=n;i++){
+            int num=i;
+            int rev=0;
+            while(num!=0){
+                int digit = num%10;
+                rev = rev*10+digit;
+                num=num/10;
+            }
+            if(rev==i) result.push_back(i);
+        }
+        return result;
+    }
+};
