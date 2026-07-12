d42e2903b345549a61fa956e5db9abaac12f4644
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-07-12 19:19:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum Of Digits/sum-of-digits.cpp
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.cpp
@@ -2,14 +2,14 @@ class Solution {
   public:
     int sumOfDigits(int n) {
         // code here
-        int  num =n;
-        int sum = 0 ;
-        while(num!=0){
-            int digit = num%10;
-            sum+=digit;
-            num = num/10;
-        }
-        return  sum;
-        
+     
+     int sum=0;
+     while(n!=0){
+         int digit = n%10;
+         sum+=digit;
+         n=n/10;
+     }
+     
+     return sum;
     }
 };
