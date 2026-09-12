a7e9d6e142511506574e9afbe040d3cc58d2d71a
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-09-12 16:42:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int minTime(vector<int>& arr, int k) {
-        int n = arr.size();
-
-        if (k >= n) {
-            return *max_element(arr.begin(), arr.end());
-        }
-
         long long low = *max_element(arr.begin(), arr.end());
         long long high = accumulate(arr.begin(), arr.end(), 0LL);
 
@@ -14,22 +8,20 @@ public:
             long long mid = low + (high - low) / 2;
 
             int workers = 1;
-            long long currentTime = 0;
+            long long time = 0;
 
-            for (int time : arr) {
-                if (currentTime + time > mid) {
+            for (int board : arr) {
+                if (time + board > mid) {
                     workers++;
-                    currentTime = time;
+                    time = board;
                 } else {
-                    currentTime += time;
+                    time += board;
                 }
             }
 
             if (workers <= k) {
-                // Possible hai, aur minimum time try karo
                 high = mid - 1;
             } else {
-                // Workers zyada chahiye, time badhao
                 low = mid + 1;
             }
         }
