98a3f096e06937e6cdb62129cba794a3be4b5e1d
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-09-13 09:36:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Greater on Right Queries/count-greater-on-right-queries.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> countGreater(vector<int> &arr, vector<int> &indices) {
+        vector<int> ans;
+
+        for (int idx : indices) {
+            int count = 0;
+
+            for (int j = idx + 1; j < arr.size(); j++) {
+                if (arr[j] > arr[idx]) {
+                    count++;
+                }
+            }
+
+            ans.push_back(count);
+        }
+
+        return ans;
+    }
+};
