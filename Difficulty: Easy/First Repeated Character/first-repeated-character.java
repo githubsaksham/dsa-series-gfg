391a4be29b75f244c2c1d860044f84fd840f6aee
Saksham Agarwal <131918063+githubsaksham@users.noreply.github.com>
2026-09-24 02:26:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Repeated Character/first-repeated-character.java
+++ Difficulty: Easy/First Repeated Character/first-repeated-character.java
@@ -1,17 +1,19 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 class Solution {
     String firstRepChar(String s) {
 
-        HashSet<Character> set = new HashSet<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
 
-            if (set.contains(s.charAt(i))) {
-                return String.valueOf(s.charAt(i));
+            char ch = s.charAt(i);
+
+            if (map.containsKey(ch)) {
+                return String.valueOf(ch);
             }
 
-            set.add(s.charAt(i));
+            map.put(ch, 1);
         }
 
         return "-1";
